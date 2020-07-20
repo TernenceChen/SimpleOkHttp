3b4a5a33a602b2cf7eb65729f2329c20d4d9ee20
ternence.chen <ternence.chen@oneplus.com>
2020-07-20 11:10:36 +0800

    Delete unless code

--- app/src/main/java/org/ternence/simpleokhttp/okhttp/RealCall.kt
+++ app/src/main/java/org/ternence/simpleokhttp/okhttp/RealCall.kt
@@ -1,16 +1,3 @@
-/*****************************************************************
- * * Copyright (C), 2020-2029, OnePlus Technology (Shenzhen) Co., Ltd
- * * VENDOR_EDIT
- * * File: - RealCall
- * * Description:
- * * Version: 1.0
- * * Date : 20年7月18日
- * * Author: ternence.chen@oneplus.com
- * *
- * * ---------------------- Revision History:----------------------
- * * <author> <date> <version> <desc>
- * * ternence.chen 20年7月18日 1.0 create
- ******************************************************************/
 package org.ternence.simpleokhttp.okhttp
 
 class RealCall(
